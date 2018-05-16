61305c7edd546c828a0e6e591a5cbd4cef527d04
fields marshall <fieldsmarshall@fieldss-MacBook-Air.local>
2018-05-16 19:08:25 -0400

    changes

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://jumprock.co/mail/PUTALIASHERE",
+        url: "https://jumprock.co/mail/magicaurus",
         type: "POST",
         data: {
           name: name,
